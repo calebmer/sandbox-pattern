6d9df830853a401917e454ac1674c0debd725819
Caleb Meredith <calebmeredith8@gmail.com>
2015-05-28 17:56:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,161 @@
+# Sandbox Pattern
+
+Working on a project where your javascript files are arbitrarily loaded? Need a tool besides CommonJS or Browserify to easily manage dependencies? Then you should consider using the sandbox pattern.
+
+The code in this repository is an implementation of the Sandbox javascript pattern as illustrated in Stoyan Stefanov's book [Javascript Patterns](http://www.amazon.com/JavaScript-Patterns-Stoyan-Stefanov/dp/0596806752).
+
+## Installation
+
+To use with NodeJS:
+```bash
+$ npm install --save sandbox-pattern
+```
+
+To use with Meteor:
+```bash
+$ meteor add calebmer:sandbox
+```
+
+## Usage
+
+A sandbox is a scoped context block with one variable passed in, the `box` variable. The easiest way to visualize how the sandbox module works is with the following (we assume `sandbox` is defined in the global scope).
+
+```javascript
+sandbox('bird', function () {
+  return {
+    chirp: function () {
+      console.log('cawcaw!');
+    }
+  };
+});
+
+sandbox(['bird'], function (box) {
+  box.bird.chirp(); // # => cawcaw!
+});
+```
+
+This may seem completely useless until you realize that the `sandbox` function allows you to define your "sandboxes" in any order. The following code will give the exact same output.
+
+```javascript
+sandbox(['bird'], function (box) {
+  box.bird.chirp(); // # => cawcaw!
+});
+
+sandbox('bird', function () {
+  return {
+    chirp: function () {
+      console.log('cawcaw!');
+    }
+  };
+});
+```
+
+### Import and Export
+
+A sandbox can be a module, and can call for modules at the same time. As long as no circular dependencies exist this will be fine. For example:
+
+```javascript
+sandbox('bird', function () {
+  return { color: 'tomato' };
+});
+
+sandbox('cat', ['bird'], function (box) {
+  return { chases: [box.bird] };
+});
+
+sandbox(['bird', 'cat'], function (box) {
+  console.log(box.bird.color === box.cat.chases[0].color); // # => true
+});
+```
+
+### Extending modules
+
+One can also extend modules. Remember, the following sandboxes can be called in any order and the same output will be realized.
+
+```javascript
+sandbox('bird', function () {
+  return { color: 'tomato' };
+});
+
+sandbox('bird', function () {
+  return {
+    chirp: function () {
+      console.log('cawcaw!');
+    }
+  };
+});
+
+sandbox(['bird'], function (box) {
+  console.log(box.bird.color); // # => tomato
+  box.bird.chirp();            // # => cawcaw!
+});
+```
+
+### Dot Syntax
+
+You can use javascript's dot syntax to define module components.
+
+```javascript
+sandbox('bird.color', function () {
+  return 'tomato';
+});
+```
+
+Will produce the same effect as:
+
+```javascript
+sandbox('bird', function () {
+  return { color: 'tomato' };
+});
+```
+
+When declaring a module as a dependency, all child modules will also be added to the dependency list. For example, if one called for `bird` and `bird.color` was defined as a module, `bird.color` would be included in the box.
+
+One advantage to using the dot syntax is you can retrieve them independently from the parent module. Again, the sandboxes can be defined in any order.
+
+```javascript
+sandbox('bird', function () {
+  return { gender: 'female' };
+});
+
+sandbox('bird.color', function () {
+  return 'tomato';
+});
+
+sandbox('bird.size', function () {
+  return 42;
+});
+
+sandbox(['bird.color'], function (box) {
+  console.log(box.bird.color);  // # => tomato
+  console.log(box.bird.gender); // # => undefined
+  console.log(box.bird.size);   // # => undefined
+});
+
+sandbox(['bird'], function (box) {
+  console.log(box.bird.color);  // # => tomato
+  console.log(box.bird.gender); // # => female
+  console.log(box.bird.size);   // # => 42
+});
+```
+
+### Error Handling
+
+On some occasions, such as when a module does not exist, or a circular dependency is found, errors will be thrown. If you want to handle the error just add another parameter to the box function. For example:
+
+```javascript
+sandbox(['doesNotExist'], function (err, box) {
+  console.log(err); // # => (some Error)
+  console.log(box); // # => undefined
+});
+```
+
+If there is no error, `err` would be undefined and `box` would have the traditional box modules.
+
+## How it Works
+
+Process loops (TODO: extend)
+
+## Is the README comprehensible?
+
+If you don't understand something, or anything seems unclear, please submit an issue.
---
+++ test/main.js
@@ -0,0 +1,167 @@
+var assert  = require('assert');
+var sandbox = require('../lib/sandbox');
+
+describe('sandbox', function () {
+  beforeEach(function () {
+    sandbox.reset();
+  });
+
+  it('loads in standard order', function (done) {
+    sandbox('module1', function () { return { hello: 'world' }; });
+
+    sandbox(['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      done();
+    });
+  });
+
+  it('loads in unstandard order', function (done) {
+    sandbox(['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      done();
+    });
+
+    sandbox('module1', function () { return { hello: 'world' }; });
+  });
+
+  it('can extend modules', function (done) {
+    sandbox('module1', function () { return { hello: 'world' }; });
+    sandbox('module1', function () { return { world: 'hello' }; });
+
+    sandbox(['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.equal(box.module1.world, 'hello');
+      done();
+    });
+  });
+
+  it('can extend modules in wierd orders', function (done) {
+    sandbox('module1', function () { return { world: 'hello' }; });
+
+    sandbox(['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.equal(box.module1.world, 'hello');
+      done();
+    });
+
+    sandbox('module1', function () { return { hello: 'world' }; });
+  });
+
+  it('enables simoultaneous import/export', function (done) {
+    sandbox('module1', function () { return { hello: 'world' }; });
+
+    sandbox('module2', ['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      return { world: 'hello' };
+    });
+
+    sandbox(['module1', 'module2'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.equal(box.module2.world, 'hello');
+      done();
+    });
+  });
+
+  it('enables simoultaneous import/export in werd orders', function (done) {
+    sandbox(['module1', 'module2'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.equal(box.module2.world, 'hello');
+      done();
+    });
+
+    sandbox('module1', function () { return { hello: 'world' }; });
+
+    sandbox('module2', ['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      return { world: 'hello' };
+    });
+  });
+
+  it('can have one module with many dependents', function (done) {
+    sandbox('module1', function () { return { hello: 'world' }; });
+
+    sandbox(['module1', 'module2'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.equal(box.module1.hello, 'world');
+      done();
+    });
+
+    sandbox('module2', ['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      return { world: 'hello' };
+    });
+  });
+
+  it('boxes that handle errors without errors is not broken', function (done) {
+    sandbox('module1', function () { return { hello: 'world' }; });
+
+    sandbox(['module1'], function (err, box) {
+      assert.ifError(err);
+      assert.equal(box.module1.hello, 'world');
+      done();
+    });
+  });
+
+  it('detects when modules don\'t exist', function (done) {
+    sandbox(['module1'], function (err, box) {
+      assert.throws(assert.ifError.bind({}, err));
+      assert.equal(box, undefined);
+      done();
+    });
+  });
+
+  it('won\'t allow circular dependencies - lv1', function () {
+    sandbox('module1', ['module1'], function () { assert(false); });
+  });
+
+  it('won\'t allow circular dependencies - lv2', function () {
+    sandbox('module1', ['module2'], function () { assert(false); });
+    sandbox('module2', ['module1'], function () { assert(false); });
+  });
+
+  it('won\'t allow circular dependencies - lv3', function () {
+    sandbox('module1', ['module3'], function () { assert(false); });
+    sandbox('module2', ['module1'], function () { assert(false); });
+    sandbox('module3', ['module2'], function () { assert(false); });
+  });
+
+  it('allows circular dependencies with an escape', function () {
+    sandbox('module1', function () { return { hello: 'world' }; });
+
+    sandbox('module1', ['module2'], function (box) {
+      assert.equal(box.module2.another, 'module');
+      return { world: 'hello' };
+    });
+
+    sandbox('module2', ['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.notEqual(box.module1.world, 'hello');
+      return { another: 'module' };
+    });
+  });
+
+  it('supports child modules', function (done) {
+    var obj = { some: 'obj' };
+
+    var func = function () {
+      return null;
+    };
+
+    sandbox('module1', function () { return { hello: 'world' }; });
+    sandbox('module1', function () { return { world: 'hello' }; });
+    sandbox('module1.func', function () { return func; });
+    sandbox('module1.obj', function () { return obj; });
+
+    sandbox(['module1'], function (box) {
+      assert.equal(box.module1.hello, 'world');
+      assert.equal(box.module1.world, 'hello');
+      assert.equal(box.module1.func, func);
+      assert.equal(box.module1.obj, obj);
+      done();
+    });
+
+    sandbox(['module1.func'], function (box) {
+      assert.equal(box.module1.func, func);
+    });
+  });
+});
